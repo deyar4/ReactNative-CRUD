049d90a7ee473de330187970f45f23bbbed8c950
deyar4 <deyarbakr4@gmail.com>
2023-01-07 07:56:54 +0300

    created RestaurantScreen & added heading image, title and description

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,6 +1,11 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React, {useLayoutEffect, useState, useEffect} from 'react'
 import { useRoute, useNavigation } from '@react-navigation/native'
+import {ArrowLeftIcon, 
+        ChevronRightIcon,
+        MapPinIcon,
+        StarIcon
+        } from "react-native-heroicons/solid"; 
 
 
 const RestaurantScreen = () => {
@@ -27,9 +32,42 @@ const RestaurantScreen = () => {
 
     }, [])    
   return (
-    <View>
-      <Text>{title}</Text>
-    </View>
+    <ScrollView>
+
+        {/* Header Img */}
+      <View className="relative">
+        <Image source = {{
+            uri: imgUrl,
+        }} 
+        className="w-full h-64 p-4"
+        />
+        <TouchableOpacity 
+        onPress={navigation.goBack}
+        className= "absolute top-14 left-5 p-2 bg-white rounded-full">
+        <ArrowLeftIcon size={30} color="#E01E3A" />
+        </TouchableOpacity>
+      </View>
+
+        {/* Title Area */}
+      <View className= "bg-white">
+        <View className= "px-4 pt-4">
+            <Text className= "text-4xl font-bold">{title}</Text>
+            <View className= "flex-row space-x-2 my-1">
+                <View className='flex-row items-center space-x-1'>
+                    <StarIcon size={20} opacity={0.5} color="green"/>
+                    <Text className= "text-green-500 font-bold text-md">{rating}</Text>
+                    <Text> |  {type}</Text>
+                    <MapPinIcon size={20} opacity={0.3} color="gray"/>
+                    <Text className= "text-gray-400 font-bold text-md">{address}</Text>
+                </View>
+            </View>
+            <Text className= "text-gray-400 text-md">{description}</Text>
+        </View>
+            <View>
+                <Text className="text-2xl font-bold px-4 pt-6 mb-3 text-gray-600">Menu</Text>
+            </View>
+      </View>
+    </ScrollView>
   )
 }
 
